3c1b979f3febc0082dae68f775d2a50a16bf03df
SiegeLord <slabode@aim.com>
2019-01-11 21:04:17 -0800

    Better compile command

--- Extensions/XortekCorporation/compile.sh
+++ Extensions/XortekCorporation/compile.sh
@@ -3,6 +3,6 @@
 set -x
 set -e
 
-wine TransCompiler.exe /input:/home/siege/Documents/Transcendence/Extensions/XortekCorporation/SiegeLordLibrary.xml /output:Extensions/sl_library.tdb
-wine TransCompiler.exe /input:/home/siege/Documents/Transcendence/Extensions/XortekCorporation/SiegeLordXortekCorporationExt.xml /output:Extensions/xortek_corporation.tdb /entities:Extensions/sl_library.tdb
-wine TransCompiler.exe /input:/home/siege/Documents/Transcendence/Extensions/XortekCorporation/SiegeLordGarnetPlayerShipExt.xml /output:Extensions/garnet_playership.tdb /entities:Extensions/sl_library.tdb
+wine TransCompiler.exe /input:/home/siege/Documents/Transcendence/Extensions/XortekCorporation/SiegeLordLibrary.xml /output:Extensions/SiegeLordLib.tdb
+wine TransCompiler.exe /input:/home/siege/Documents/Transcendence/Extensions/XortekCorporation/SiegeLordXortekCorporationExt.xml /output:Extensions/XortekCorporation.tdb /entities:Extensions/SiegeLordLib.tdb
+wine TransCompiler.exe /input:/home/siege/Documents/Transcendence/Extensions/XortekCorporation/SiegeLordGarnetPlayerShipExt.xml /output:Extensions/GarnetPlayership.tdb /entities:Extensions/SiegeLordLib.tdb
