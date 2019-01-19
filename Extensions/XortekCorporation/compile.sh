968f7c239cb31c70f6233ce91d41aa649bdc1fb1
SiegeLord <slabode@aim.com>
2019-01-19 12:56:57 -0800

    Adjust compile script, more work on the capsule

--- Extensions/XortekCorporation/compile.sh
+++ Extensions/XortekCorporation/compile.sh
@@ -3,6 +3,16 @@
 set -x
 set -e
 
-wine TransCompiler.exe /input:/home/siege/Documents/Transcendence/Extensions/XortekCorporation/SiegeLordLibrary.xml /output:Extensions/SiegeLordLib.tdb
-wine TransCompiler.exe /input:/home/siege/Documents/Transcendence/Extensions/XortekCorporation/SiegeLordXortekCorporationExt.xml /output:Extensions/XortekCorporation.tdb /entities:Extensions/SiegeLordLib.tdb
-wine TransCompiler.exe /input:/home/siege/Documents/Transcendence/Extensions/XortekCorporation/SiegeLordGarnetPlayerShipExt.xml /output:Extensions/GarnetPlayership.tdb /entities:Extensions/SiegeLordLib.tdb
+if [[ -z "${BASE}" ]]; then
+	if uname -s | grep --quiet MINGW64 ; then
+		BASE=$(cygpath -w $(dirname $0))
+		WINE=
+	else
+		BASE=$(dirname $0)
+		WINE=wine
+	fi
+fi
+
+${WINE} ./TransCompiler.exe /input:${BASE}/SiegeLordLibrary.xml /output:Extensions/SiegeLordLib.tdb
+${WINE} ./TransCompiler.exe /input:${BASE}/SiegeLordXortekCorporationExt.xml /output:Extensions/XortekCorporation.tdb /entities:Extensions/SiegeLordLib.tdb
+${WINE} ./TransCompiler.exe /input:${BASE}/SiegeLordGarnetPlayerShipExt.xml /output:Extensions/GarnetPlayership.tdb /entities:Extensions/SiegeLordLib.tdb
